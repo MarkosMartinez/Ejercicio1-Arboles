95d435265424b6679c0f6ad66a47156a1ffebc6a
Markos Martinez <Markos Martinez@HZ114826.H012108.NET>
2023-01-17 14:28:04 +0100

    Primera opcion del menu planteada y conexion a la BD configurada.

--- src/clases/GestorArboles.java
+++ src/clases/GestorArboles.java
@@ -1,11 +1,42 @@
 package clases;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+/*import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;*/
 import java.util.Scanner;
 
 public class GestorArboles {
-
+	
+	private static final String HOST = "localhost";
+	private static final String BBDD = "eh_garden";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+	
 	public void run() {
 	
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://" + HOST + "/" + BBDD, USERNAME, PASSWORD);
+			
+			Statement st = con.createStatement(); //Crear el ejecutor de sentencias
+			/*String sentenciaInsert = "INSERT INTO animales (nombre) VALUES ('Test');";
+			st.execute(sentenciaInsert);*/  //No es necesario ";" si solo es una linea.
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		final int INSERTAR_ARBOL = 1;
 		final int ELIMINAR_ARBOL = 2;
 		final int MODIFICAR_INFOR = 3;
@@ -28,6 +59,17 @@ public class GestorArboles {
 			switch (opcion_menu) {
 			case INSERTAR_ARBOL:
 				System.out.println("Opcion escogida: " + INSERTAR_ARBOL + "\n");
+				
+				System.out.println("Escribe el nombre comun del árbol: ");
+				String nombreComun = scan.nextLine();
+				System.out.println("Escribe el nombre cientifico del árbol: ");
+				String nombreCientifico = scan.nextLine();
+				System.out.println("Escribe el nombre del habitat del árbol: ");
+				String habitat = scan.nextLine();
+				System.out.println("Escribe la altura del árbol: ");
+				int altura = Integer.parseInt(scan.nextLine());
+				System.out.println("Escribe el origen del árbol: ");
+				String origen = scan.nextLine();
 
 				break;
 			case ELIMINAR_ARBOL:
